253d87169cd5864435d9bb24e7f6a59f1428f284
Ilya Kalugin <kalugin.ilya@gmail.com>
2019-06-22 14:00:20 +0300

    Добавление пользователей и статей

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, redirect, request
-from webapp.model import db
+from flask import Flask, render_template, url_for, redirect, request, flash
+from webapp.form import UserForm, EventForm
+from webapp.model import db, Event, User, Position_type
 
 def create_app():
     app = Flask(__name__)
@@ -12,22 +13,54 @@ def create_app():
         hello_text = 'Hello, world!'
         return render_template('index.html', title=title, hello_text=hello_text)
 
-    @app.route('/success/<what>')
-    def success(what):
-        return render_template('success.html', what=what)
-
     @app.route('/event', methods=['GET', 'POST'])
     def event():
         title = 'Статьи'
-        if request.method == 'POST':
-            return redirect(url_for('success', what='Cтатья'))
-        return render_template('event.html', title=title)
+        positions = [(p.id, p.position_name) for p in Position_type.query.all()]
+        event_form = EventForm(request.form)
+        event_form.position_type.choices = positions
+        events_list = Event.query.all()
+
+        if event_form.validate_on_submit():
+            event_name = event_form.event_name.data
+            text = event_form.text.data
+            interval = event_form.interval.data
+            positions = Position_type.query.filter(Position_type.id.in_(event_form.position_type.data)).all()
+            new_event = Event(event_name=event_name,
+                              text=text,
+                              interval=interval,
+                              positions=positions)
+            db.session.add(new_event)
+            db.session.commit()
+            flash('Статья успешно добавлена.')
+            return redirect(url_for('event'))
+ 
+        return render_template('event.html', title=title, form=event_form, events_list=events_list)
 
     @app.route('/users', methods=['GET', 'POST'])
     def users():
         title = 'Пользователи'
-        if request.method == 'POST':
-            return redirect(url_for('success', what='Пользователь'))
-        return render_template('users.html', title=title)
+        positions = [(p.id, p.position_name) for p in Position_type.query.all()]
+        user_form = UserForm(request.form)
+        user_form.position_type.choices = positions
+        users_list = User.query.all()
+
+        if user_form.validate_on_submit():
+            first_name = user_form.first_name.data
+            last_name = user_form.last_name.data
+            slack_id = user_form.slack_id.data
+            position_type = user_form.position_type.data
+            start_date = user_form.start_date.data
+            new_user = User(first_name=first_name,
+                            last_name=last_name, 
+                            slack_id=slack_id, 
+                            start_date=start_date, 
+                            position_type=position_type)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Пользователь успешно добавлен.')
+            return redirect(url_for('users'))
+
+        return render_template('users.html', title=title, form=user_form, users_list=users_list)
 
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -8,17 +8,35 @@ class User(db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     slack_id = db.Column(db.String, nullable=False, unique=True)
-    position_type = db.Column(db.Integer, nullable=False)
-    start_date = db.Column(db.DateTime, nullable=False)
+    position_type = db.Column(db.Integer, db.ForeignKey('position_type.id'), nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+
+    # Связь с таблицей position_type.
+    # Позволяет вызвать всех пользователей должности Position_type.users
+    position = db.relationship('Position_type',
+        backref=db.backref('users', lazy='dynamic') 
+    )
 
     def __repr__(self):
         return '<User {} {} {}>'.format(self.first_name, self.last_name, self.start_date)
 
+# Дополнительная таблица для связи event и position_type
+position_event = db.Table('position_event',
+    db.Column('position_type', db.Integer, db.ForeignKey('position_type.id')),
+    db.Column('event', db.Integer, db.ForeignKey('event.id'))
+)
+
 class Event(db.Model):
     __tablename__ = 'event'
     id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String, nullable=False)
     text = db.Column(db.Text, nullable=False)
     interval = db.Column(db.Integer, nullable=False)
+
+    # Связь с многие-ко-многим таблицы event и position_type
+    positions = db.relationship('Position_type', secondary=position_event,
+        backref=db.backref('events', lazy='dynamic')
+    )
 
     def __repr__(self):
         return '<Event {} {}>'.format(self.text, self.interval)
@@ -31,22 +49,21 @@ class Position_type(db.Model):
     def __repr__(self):
         return '<Position type {}>'.format(self.position_name)
 
-class Position_event(db.Model):
-    __tablename__ = 'position_event'
-    id = db.Column(db.Integer, primary_key=True)
-    position_type = db.Column(db.Integer, nullable=False)
-    event = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return '<Position and event {} {}>'.format(self.position_type, self.event)
-
 class Schedule(db.Model):
     __tablename__ = 'schedule'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    event = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
     delivery_date = db.Column(db.DateTime, nullable=False)
     delivery_status = db.Column(db.String, nullable=True)
+
+    # Связь с таблицей user. Позволяет вызвать все расписание для пользователя User.shedules
+    user = db.relationship('User',
+        backref=db.backref('schedules', lazy='dynamic')
+    )
+    # Связь с таблицей event. Позволяет вызвать все расписания связанные с событием Event.shedules
+    event = db.relationship('Event',
+        backref=db.backref('schedules', lazy='dynamic'))
 
     def __repr__(self):
         return '<Schedule {} {} {}>'.format(self.user_id, self.delivery_date, self.delivery_status)
